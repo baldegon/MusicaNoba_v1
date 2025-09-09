62c60d4af5b8c45c2d9de419f0f580e22c6e184a
baldegon <bladimirzink7@gmail.com>
2025-09-09 18:29:09 -0300

    Add project files.

---
+++ MusicaNoba_v1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicaNoba_v1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MusicaNoba_v1/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicaNoba_v1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
